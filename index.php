81e8021469bcab5f7cf79e1b2037bd67cd28c5f3
Timo <81316147+Waterbroodje@users.noreply.github.com>
2023-11-18 16:06:56 +0100

    Commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!-- index.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cat Sharing</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+</head>
+<body>
+    <h1>Generate & Share cats. <strong class="color">Easy & Free</strong>.</h1>
+    <div class="cat">
+        <img id="catImage">
+        <div class="cat-buttons">
+            <button onclick="generateNewCat()">Generate New Cat</button>
+            <button onclick="shareCat()">Share</button>
+        </div>
+    </div>
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,70 @@
+function generateNewCat() {
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            let data = JSON.parse(xhr.responseText);
+            let newCatImage = document.getElementById("catImage");
+
+            newCatImage.src = data[0].url;
+
+            window.currentCatID = data[0].id;
+        }
+    };
+
+    xhr.open("GET", "https://api.thecatapi.com/v1/images/search/", true);
+    xhr.send();
+}
+/*
+function shareCat() {
+    let catID = window.currentCatID;
+    let shareURL = "https://cdn2.thecatapi.com/images/" + catID + ".jpg";
+
+    // Copy the URL to the clipboard
+    navigator.clipboard.writeText(shareURL).then(r => {
+        let popup = document.createElement("div");
+        popup.className = "custom-popup";
+        popup.textContent = "URL copied to clipboard!";
+
+        // Append the popup to the body
+        document.body.appendChild(popup);
+
+        // Remove the popup after a short delay (e.g., 3 seconds)
+        setTimeout(function () {
+            document.body.removeChild(popup);
+        }, 1000);
+    });
+}
+ */
+
+/*
+function shareCat() {
+    let catID = window.currentCatID;
+    // Redirect to the shared cat page
+    window.location.href = `share.php?catId=${catID}`;
+}
+ */
+
+function shareCat() {
+    let catID = window.currentCatID;
+    let shareURL = `https://catsharing.waterbroodje.nl/${catID}`; // Replace with your actual domain
+
+    // Copy the URL to the clipboard
+    navigator.clipboard.writeText(shareURL).then(() => {
+        let popup = document.createElement("div");
+        popup.className = "custom-popup";
+        popup.textContent = "URL copied to clipboard!";
+
+        // Append the popup to the body
+        document.body.appendChild(popup);
+
+        // Remove the popup after a short delay (e.g., 3 seconds)
+        setTimeout(function () {
+            document.body.removeChild(popup);
+        }, 1000);
+    }).catch(error => {
+        console.error('Error copying URL to clipboard:', error);
+    });
+}
+
+
+generateNewCat();
